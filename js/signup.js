33d714d896ae728ff2767e73b43f7a9e60989dc4
iulia2100 <hosuiulia03@yahoo.com>
2023-01-18 16:26:39 +0100

    Changed to regex validation

--- js/signup.js
+++ js/signup.js
@@ -1,17 +1,9 @@
 var checkUsername = () =>  {
     var userId = document.getElementsByName("id")[0].value;
-    if (userId == null || userId == "") {
+    if (!/^[A-Z].{4,11}$/.test(userId))   {
         document.getElementsByName("id")[0].className = "red-input";
         document.getElementById("usernameError").innerHTML = 
-        "<div class='wrong'>Please fill out this field.</div>";
-    } else if (userId.length < 5 || userId.length > 12)    {
-        document.getElementsByName("id")[0].className = "red-input";
-        document.getElementById("usernameError").innerHTML = 
-        "<div class='wrong'>Username length must be between 5 and 12 characters.</div>";
-    } else if (!/^[A-Z]*$/.test(userId.charAt(0)))   {
-        document.getElementsByName("id")[0].className = "red-input";
-        document.getElementById("usernameError").innerHTML = 
-        "<div class='wrong'>Username must start with a capital letter.</div>";
+        "<div class='wrong'>Username must be between 5 and 12 characters and start with a capital letter.</div>";
     } else  {
         document.getElementsByName("id")[0].className = "green-input";
         document.getElementById("usernameError").innerHTML = 
@@ -23,15 +15,10 @@ var checkUsername = () =>  {
 
 var checkName = a => {
     var name = document.getElementsByName("name")[a].value;
-    if (name == null || name == "") {
+    if (!/^[A-Z][A-Za-z]*$/.test(name))   {
         document.getElementsByName("name")[a].className = "red-input";
-        document.getElementsByName("nameError")[a].innerHTML = "<div class='wrong'>Please fill out this field.</div>";
-    } else if (!/^[A-Za-z]*$/.test(name))   {
-        document.getElementsByName("name")[a].className = "red-input";
-        document.getElementsByName("nameError")[a].innerHTML = "<div class='wrong'>Name should only contain letters.</div>";
-    } else if (!/^[A-Z]*$/.test(name.charAt(0)))    {
-        document.getElementsByName("name")[a].className = "red-input";
-        document.getElementsByName("nameError")[a].innerHTML = "<div class='wrong'>Name should start with a capital letter.</div>";
+        document.getElementsByName("nameError")[a].innerHTML = 
+        "<div class='wrong'>Name should start with a capital letter and only contain letters.</div>";
     } else  {
         document.getElementsByName("name")[a].className = "green-input";
         document.getElementsByName("nameError")[a].innerHTML = "<div class='correct'>Looks good!</div>";
@@ -42,10 +29,7 @@ var checkName = a => {
 
 var checkEmail = () =>  {
     var email = document.getElementsByName("email")[0].value;
-    if (email == null || email == "")   {
-        document.getElementsByName("email")[0].className = "red-input";
-        document.getElementById("emailError").innerHTML = "<div class='wrong'>Please fill out this field.</div>"; 
-    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email))    {
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email))    {
         document.getElementsByName("email")[0].className = "red-input";
         document.getElementById("emailError").innerHTML = 
             "<div class='wrong'>Please input a valid email address.</div>";
@@ -60,10 +44,7 @@ var checkEmail = () =>  {
 
 var checkZipCode = () =>    {
     var zip = document.getElementsByName("zip")[0].value;
-    if (zip == null || zip == "")   {
-        document.getElementsByName("zip")[0].className = "red-input";
-        document.getElementById("zipError").innerHTML = "<div class='wrong'>Please fill out this field.</div>"; 
-    } else if (!/^[1-9][0-9]{3} ?[a-z]{2}$/i.test(zip))  {
+    if (!/^[1-9][0-9]{3} ?[a-z]{2}$/i.test(zip))  {
         document.getElementsByName("zip")[0].className = "red-input";
         document.getElementById("zipError").innerHTML = 
             "<div class='wrong'>Please input a valid zip code.</div>"; 
@@ -78,7 +59,7 @@ var checkZipCode = () =>    {
 
 var checkCountry = () =>    {
     var country = document.getElementById("country").value;
-    if (country == null || country == "")   {
+    if (!/.$/.test(country))   {
         document.getElementById("country").className = "red-input";
         document.getElementById("countryError").innerHTML = "<div class='wrong'>Please select your country.</div>"; 
     } else  {
@@ -91,7 +72,7 @@ var checkCountry = () =>    {
 
 var checkLang = () =>    {
     var lang = document.getElementById("lang").value;
-    if (lang == null || lang == "")   {
+    if (!/.$/.test(lang))   {
         document.getElementById("lang").className = "red-input";
         document.getElementById("langError").innerHTML = "<div class='wrong'>Please select your preferred language.</div>"; 
     } else  {
@@ -104,7 +85,7 @@ var checkLang = () =>    {
 
 var checkForm = () =>   {
     if(checkName(0) & checkName(1) & checkUsername() & checkEmail() 
-        & checkLang() & checkCountry() & checkZipCode() & checkPassword(0)
+        & checkLang() & checkCountry() & checkZipCode() & checkSex() & checkPassword(0)
         && verifyPassword(0, 1)) {
         document.getElementById("signup").reset();
     }
@@ -112,16 +93,16 @@ var checkForm = () =>   {
 
 var checkPassword = p => {
     var psw = document.getElementsByName("psw")[p].value;
-    if(psw == null || psw == "") {
+    if(!/.$/.test(psw)) {
         document.getElementsByName("psw")[p].className = "red-input";
         document.getElementsByName("pswError")[p].innerHTML = "<div class = 'wrong'>Please fill out this field.</div>";
-    } else if(psw.length < 12) {
+    } else if(!/.{12,}$/.test(psw)) {
         document.getElementsByName("psw")[p].className = "red-input";
         document.getElementsByName("pswError")[p].innerHTML = "<div class = 'wrong'>Password should be at least 12 characters long.</div>";
     } else if(!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/.test(psw)) {
             document.getElementsByName("psw")[p].className = "red-input";
             document.getElementsByName("pswError")[p].innerHTML = "<div class = 'wrong'>Password should contain a combination of uppercase letters, lowercase letters, numbers, and symbols.</div>";
-    } else if(psw.length >= 12 && psw.length < 14) {
+    } else if(/^.{12,14}$/.test(psw)) {
         document.getElementsByName("psw")[p].className = "yellow-input";
         document.getElementsByName("pswError")[p].innerHTML = "<div class = 'weak'>Your pawwsord is weak.</div>";
     } else {
@@ -134,7 +115,7 @@ var verifyPassword = (p, q) => {
     var psw1 = document.getElementsByName("psw")[p].value;
     checkPassword(0);
     var psw2 = document.getElementsByName("psw")[q].value;
-    if(psw2 == null || psw2 == "") {
+    if(!/.$/.test(psw2)) {
         document.getElementsByName("psw")[q].className = "red-input";
         document.getElementsByName("pswError")[q].innerHTML = "<div class = 'wrong'>Please fill out this field.</div>";
     } else if(psw1 !== psw2) {
@@ -145,4 +126,10 @@ var verifyPassword = (p, q) => {
         document.getElementsByName("pswError")[q].innerHTML = "<div class = 'correct'>The passswords match!</div>";
     }
 
+}
+
+var checkSex = () =>    {
+    if (document.querySelector('input[name="sex"]:checked') == null)    {
+        document.getElementById("sexError").innerHTML = "<div class = 'wrong'>Please select gender.</div>"
+    }
 }
